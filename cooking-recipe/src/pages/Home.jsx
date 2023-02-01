e6c62b0773df61b41523afc8ad4c7b56d3d67155
Shinya-Fukuta0116 <alleyezon.djwanted@gmail.com>
2023-02-02 06:13:34 +0900

    first commit

---
+++ cooking-recipe/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Veggie from "../compornents/Veggie";
+import Popular from "../compornents/Popular";
+
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <Veggie />
+      <Popular />
+    </div>
+  )
+}
+export default Home
